d278a961b019e17cea7a6272c6336cfc4181fd7f
Mostafa Rizk <m.rizk.student@gmail.com>
2018-03-10 11:56:41 -0700

    Created .class file to print letter

---
+++ Tester.java
@@ -0,0 +1,15 @@
+//Author: Mostafa Rizk
+//Date created: Mar 10, 2018
+public class Tester
+{
+	public static void main(String args[])
+	{
+		System.out.println("From: Bill Smith");
+		System.out.println("Address: Dell Computer, Bldg 13");
+		System.out.println("Date: April 12, 2005");
+		System.out.println();
+		System.out.println("To: Jack Jones");
+		System.out.println();
+		System.out.println("Message: Help! I'm trapped inside a computer!");
+	}
+}
